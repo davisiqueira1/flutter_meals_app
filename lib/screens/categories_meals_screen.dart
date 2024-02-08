9f14ffdfb798d096486b9af364b4a61d0acbb571
Davi Siqueira <davisvsiqueira@gmail.com>
2024-02-08 15:08:05 -0300

    feat: create MealItem component

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/components/meal_item.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
@@ -30,7 +31,7 @@ class CategoriesMealsScreen extends StatelessWidget {
         body: ListView.builder(
           itemCount: categoryMeals.length,
           itemBuilder: (context, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(meal: categoryMeals[index]);
           },
         ));
   }
