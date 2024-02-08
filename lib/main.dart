16ed1bf6c184e7c44a63a6492af2dc0b03ea8ad1
Davi Siqueira <davisvsiqueira@gmail.com>
2024-02-08 10:06:19 -0300

    feat: create app theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final ThemeData theme = ThemeData();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Lets cook?",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: theme.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontSize: 20,
+            fontFamily: "RobotoCondensed",
+            color: Colors.black,
+          ),
+          labelLarge: const TextStyle(
+            fontFamily: "Raleway",
+          ),
+        ),
       ),
       home: const CategoriesScreen(),
     );
