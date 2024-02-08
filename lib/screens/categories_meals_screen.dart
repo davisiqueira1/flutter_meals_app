b9e08c4563884fcbb20d6c52ca2f1c836260f43f
Davi Siqueira <davisvsiqueira@gmail.com>
2024-02-08 12:05:01 -0300

    feat: access meals by category id in category meals screen

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({super.key});
@@ -7,18 +9,29 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final List<Meal> categoryMeals = dummyMeals
+        .where(
+          (Meal m) => m.categories.contains(category.id),
+        )
+        .toList();
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Colors.white,
-        title: const Text(
-          "Recipes",
-          style: TextStyle(
-            fontSize: 24,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          foregroundColor: Colors.white,
+          title: const Text(
+            "Recipes",
+            style: TextStyle(
+              fontSize: 24,
+            ),
           ),
         ),
-      ),
-      body: Center(child: Text(category.title)),
-    );
+        body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index) {
+            return Text(categoryMeals[index].title);
+          },
+        ));
   }
 }
