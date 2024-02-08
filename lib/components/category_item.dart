e5bd8688fdca93b8a919fc14c1ea46199c0d7128
Davi Siqueira <davisvsiqueira@gmail.com>
2024-02-08 10:06:43 -0300

    feat: add new style to CategoryItem

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -8,11 +8,18 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Card(
-        elevation: 2,
-        color: category.color,
-        child: Text(category.title),
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(colors: [
+          category.color.withOpacity(0.8),
+          category.color,
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge,
       ),
     );
   }
