2c50e75e80de0887760b9514caaf75b84fecffc2
Davi Siqueira <davisvsiqueira@gmail.com>
2024-02-08 08:37:43 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Lets cook?",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Lets cook?",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Center(
+          child: Text(
+            "Placeholder",
+          ),
+        ),
+      ),
+    );
+  }
+}
