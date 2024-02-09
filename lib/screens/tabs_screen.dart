838f2e39b178d8d97df2b7286887a068d3e000dc
Davi Siqueira <davisvsiqueira@gmail.com>
2024-02-09 09:22:15 -0300

    feat: change TabBar to BottomNavigationBar on home

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,45 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favorite_meals_screen.dart';
 
-class TabScreen extends StatelessWidget {
+class TabScreen extends StatefulWidget {
   const TabScreen({super.key});
 
   @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, Object>> _screens = [
+    {"title": "Categories List", "screen": const CategoriesScreen()},
+    {"title": "My Favorite Meals", "screen": const FavoriteMealsScreen()}
+  ];
+
+  void _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // 2 tabs
-      child: Scaffold(
-        backgroundColor: Theme.of(context).canvasColor,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          foregroundColor: Colors.white,
-          title: const Text(
-            "Let's cook?",
-            style: TextStyle(fontSize: 24),
-          ),
-          bottom: TabBar(
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white,
-            indicatorColor: Theme.of(context).colorScheme.secondary,
-            tabs: const <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: "Favorites",
-              ),
-            ],
-          ),
+    return Scaffold(
+      backgroundColor: Theme.of(context).canvasColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        title: Text(
+          _screens[_selectedScreenIndex]["title"] as String,
+          style: const TextStyle(fontSize: 24),
         ),
-        body: const TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoriteMealsScreen(),
-          ],
-        ),
+      ),
+      body: _screens[_selectedScreenIndex]["screen"] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedScreenIndex,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorites",
+          ),
+        ],
       ),
     );
   }
 }
+
+        // bottom: TabBar(
+        //   labelColor: Colors.white,
+        //   unselectedLabelColor: Colors.white,
+        //   indicatorColor: Theme.of(context).colorScheme.secondary,
+        //   tabs: const <Widget>[
+        //     Tab(
+        //       icon: Icon(Icons.category),
+        //       text: "Categories",
+        //     ),
+        //     Tab(
+        //       icon: Icon(Icons.favorite),
+        //       text: "Favorites",
+        //     ),
+        //   ],
+        // ),
+      // body: const TabBarView(
+      //   children: <Widget>[
+      //     CategoriesScreen(),
+      //     FavoriteMealsScreen(),
+      //   ],
+      // ),
